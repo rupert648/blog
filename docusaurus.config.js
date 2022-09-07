bd6114afc8573d31665fbed7ff68c8e4f34860e6
rupert648 <47698091+rupert648@users.noreply.github.com>
2022-09-07 17:29:27 +0100

    updated authors and icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://github.com/rupert648.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -53,7 +53,7 @@ const config = {
         title: 'Rupert Carr',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'https://github.com/rupert648.png',
         },
         items: [
           {
